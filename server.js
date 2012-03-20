477189cc844ad0d11f273c1791220af6ed7cccc1
Moriyuki HIRATA <moriyuki@gmail.com>
2012-03-20 22:50:48 +0900

    exportして、モジュール化した。

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var http = require('http')
 
-function onRequest(req, res){
-  console.log("Request received.");
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello node.js!!\n');
+function start() {
+  function onRequest(req, res){
+    console.log("Request received.");
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello node.js!!\n');
+  }
+
+  http.createServer(onRequest).listen(8000);
+  console.log("Server has started.");
 }
 
-http.createServer(onRequest).listen(8000);
-
-console.log("Server has started.");
-
+exports.start = start;
